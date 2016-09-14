69ee0d96301524a846f3b4f91c3a0dd038617127
“Jaime” <jtorres61@cuc.edu.co>
2016-09-13 19:02:05 -0500

    Clase creada y todos los metodos creadas y terminados - los de convertir a mixto

---
+++ src/clases/Mixto.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author jaime
+ */
+public class Mixto {
+
+    private int entero;
+    private int numerador;
+    private int denominador;
+
+    public Mixto(int entero, int numerador, int denominador) {
+        this.entero = entero;
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+
+    public int getEntero() {
+        return entero;
+    }
+
+    public void setEntero(int entero) {
+        this.entero = entero;
+    }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setDenominador(int denominador) {
+        this.denominador = denominador;
+    }
+
+    public Mixto Sumar(Mixto m2) {
+        int entero = this.getEntero() + m2.getEntero();
+        int numerador = this.numerador * m2.denominador + this.denominador * m2.numerador;
+        int denominador = this.denominador * m2.denominador;
+        Mixto m = new Mixto(entero, numerador, denominador);
+        return m;
+    }
+    
+    public Mixto Restar(Mixto m2){
+        int entero = this.getEntero() - m2.getEntero();
+        int numerador =this.getNumerador() * m2.getDenominador() - this.getDenominador() * m2.getNumerador();
+        int denominador = this.getDenominador() * m2.getDenominador();
+        Mixto m = new Mixto(entero, numerador, denominador);
+        return m;
+    }
+
+    public Mixto Multiplicacion(Mixto m2){
+        int entero = this.entero * m2.getEntero();
+        int numerador = this.getNumerador() * m2.getNumerador();
+        int denominador = this.getDenominador() * m2.getDenominador();
+        Mixto m = new Mixto(entero, numerador, denominador);
+        return m;
+    }
+    
+    public Mixto Dividir(Mixto m2){
+        int entero = this.getEntero() / m2.getEntero();
+        int numerador = this.getNumerador() * m2.getDenominador();
+        int den = this.denominador * m2.getDenominador();
+        Mixto m = new Mixto(entero,numerador,denominador);
+        return m;
+    }
+}
